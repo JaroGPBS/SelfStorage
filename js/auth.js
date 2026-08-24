97aa5e5770a7292c290dd38af77008b22af4a755
Moorwen <moorwen1980@gmail.com>
2026-08-24 21:54:02 +0200

    Oznacz poprawkę centralnego komunikatu jako v0.16

--- js/auth.js
+++ js/auth.js
@@ -5,7 +5,7 @@ const INVALID_AUTH_MESSAGE = 'Nieprawidłowe dane. Skontaktuj się z administrat
 function setDisplayedVersion() {
   for (const element of document.querySelectorAll('body > div')) {
     if (/^v0\.\d+$/.test(element.textContent?.trim() || '')) {
-      element.textContent = 'v0.15';
+      element.textContent = 'v0.16';
       element.style.fontSize = '13px';
       element.style.fontWeight = '600';
       element.style.opacity = '.85';
